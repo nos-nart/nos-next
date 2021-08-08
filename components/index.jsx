c813ab3fff469d518d14c1e6f68a755379ea1025
nos-nart <nosnart1510@gmail.com>
2021-08-08 19:24:53 +0700

    [-] gg

--- components/index.jsx
+++
@@ -1,5 +0,0 @@
-export * from './button-outline';
-export * from './testimoni';
-export * from './trusted';
-export * from './hero';
-export * from './service';
--- components/svgs/logo.jsx
+++
@@ -1,8 +0,0 @@
-export const Logo = (props) => (
-  <svg {...props} viewBox="0 0 158 148" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <circle r="35.4273" transform="matrix(0.258819 0.965926 0.99994 0.0109953 92.8612 38.8458)" stroke="#FFC300" strokeWidth="8.67243"/>
-    <circle r="42.9397" transform="matrix(0.866025 -0.5 -0.248183 -0.968713 101.191 77.9962)" stroke="#00D4E6" strokeWidth="8.67243"/>
-    <circle r="49.2079" transform="matrix(0.965926 0.258819 0.509492 -0.860475 79 65.6381)" stroke="#FF3E80" strokeWidth="8.67243"/>
-    <circle r="27.1007" transform="matrix(0.965926 -0.258819 0.0109953 -0.99994 104.912 59.5499)" stroke="#5B68DC" strokeWidth="8.67243"/>
-  </svg>
-)
